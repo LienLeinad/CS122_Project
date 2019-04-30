878ae45a7d333f6991240c4f59b9f969fe652671
LienLeinad <neil.bau@tista.net>
2019-04-30 16:55:09 +0800

    made a dynamic log-in system, will redirect you to a website with the proper name on the name tab, website differs from tutor to student.

--- my_app/views.py
+++ my_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import SampleModel
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     name_list = SampleModel.objects.all()
@@ -10,6 +11,11 @@ def index(request):
     context = {'name_list':name_list,}
     return render(request, 'template.html',context)
 
+def invalid_login(request):
+    if not request.user.is_authenticated:
+        return render(request,'login_error.html')
+    else:
+        return redirect('user_login')
 
 def register(request):
     if request.method == "POST":
@@ -19,25 +25,45 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account Created for {username}! go ahead and log in!')
-            return redirect('login')
+            return redirect('register')
     else:
         form = UserRegisterForm()
     context = {'form':form,}
     return render(request,'register.html', context)
 
-# def login(request):
-#     user_list = UserModel.objects.all()
-#     context = {'user_list':user_list}
-#     return render(request,'login.html',context)
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('invalid_login')
+    else:
+        user = request.user
+        user_type = user.user_type
+        if user_type == 'ST':
+            first_name = user.first_name
+            last_name = user.last_name
+            context = {'first_name': first_name, 'last_name':last_name}
 
-# def user_page(request, user_type, UserID):
-#     User = UserModel.objects.get(UserID = UserID)
-#     if 
+            return render(request,'Home/home_student.html', context)
+        elif user_type == 'TU':
+            first_name = user.first_name
+            last_name = user.last_name
+            context = {'first_name': first_name, 'last_name':last_name}
+            return render(request,'Home/home_tutor.html', context)
+    
 
+def login(request):
+    if not request.user.is_authenticated:
+        return redirect('invalid_login')
+    else:
+        user = request.user
+        user_type = user.user_type
+        if user_type == 'ST':
+            first_name = user.first_name
+            last_name = user.last_name
+            context = {'first_name': first_name, 'last_name':last_name}
 
-# def user_page(request):
-#     # User = UserModel.objects.get(user_ID = user_ID)
-#     # first_name = User.first_name
-#     # last_name = User.last_name
-#     # context = {'first_name':first_name, 'last_name':last_name}
-#     return render(request, 'tutorProfile_tutor.html')
+            return render(request,'Home/home_student.html', context)
+        elif user_type == 'TU':
+            first_name = user.first_name
+            last_name = user.last_name
+            context = {'first_name': first_name, 'last_name':last_name}
+            return render(request,'Home/home_tutor.html', context)
